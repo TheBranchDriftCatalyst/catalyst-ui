e91184454ca510d3751d34c67e967255ddca2155
DJ Daniels <dj.daniels@upstart.com>
2023-09-02 14:05:42 -0600

    stable

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,13 +1,22 @@
 import type { StorybookConfig } from "@storybook/react-webpack5";
+import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
 
 const config: StorybookConfig = {
+  webpackFinal: async (config) => {
+    config.resolve.plugins = [
+      ...(config.resolve.plugins || []),
+      new TsconfigPathsPlugin({
+        extensions: config.resolve.extensions,
+      }),
+    ];
+    return config;
+  },
   stories: [
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
     '@storybook/addon-jest',
     '@storybook/addon-storysource',
