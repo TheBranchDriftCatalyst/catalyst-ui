afc445b31e2bf93d7eff1239d86725b8b8cf25a9
TheBranchDriftCatalyst <djdanielsh@gmail.com>
2024-07-31 18:22:27 +0000

    checkpoint for github repo refactoring

--- lib/components/NavigationHeader/NavigationItem.tsx
+++ lib/components/NavigationHeader/NavigationItem.tsx
@@ -1,31 +1,38 @@
-import { NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuTrigger } from "@/catalyst-ui/ui/navigation-menu";
+import {
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuTrigger,
+} from "@/catalyst-ui/ui/navigation-menu";
 import { cn } from "@/catalyst-ui/utils";
 import React, { useMemo } from "react";
 
-export const NavigationListItem = React.forwardRef<React.ElementRef<"a">, React.ComponentPropsWithoutRef<"a"> & { comp?: React.ElementType}>(
-  ({ className, title, children, comp, ...props }, ref) => {
-    const Comp = comp || "a";
-    return (
-      <li>
-        <NavigationMenuLink asChild>
-          <Comp
-            ref={ref}
-            className={cn(
-              "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-              className
-            )}
-            {...props}
-          >
-            <div className="text-sm font-medium leading-none">{title}</div>
-            <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">{children}</p>
-          </Comp>
-        </NavigationMenuLink>
-      </li>
-    );
-  }
-);
+export const NavigationListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a"> & { comp?: React.ElementType }
+>(({ className, title, children, comp, ...props }, ref) => {
+  const Comp = comp || "a";
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <Comp
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className,
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </Comp>
+      </NavigationMenuLink>
+    </li>
+  );
+});
 NavigationListItem.displayName = "NavigationListItem";
-
 
 export interface NavigationItemBaseProps {
   title: string;
@@ -35,7 +42,8 @@ export interface NavigationItemChildrenProps extends NavigationItemBaseProps {
   children?: React.ReactNode;
 }
 
-export interface NavigationItemListChildrenProps extends NavigationItemBaseProps {
+export interface NavigationItemListChildrenProps
+  extends NavigationItemBaseProps {
   links?: NavigationListLinks[];
 }
 
@@ -45,41 +53,49 @@ export interface NavigationListLinks {
   description: string;
 }
 
-export const _sampleLinkObjects: NavigationListLinks[] = Array.from({ length: 10 }, (_, i) => ({
-  title: `Link ${i + 1}`,
-  href: "/",
-  description: "A description of the link",
-}));
+export const _sampleLinkObjects: NavigationListLinks[] = Array.from(
+  { length: 10 },
+  (_, i) => ({
+    title: `Link ${i + 1}`,
+    href: "/",
+    description: "A description of the link",
+  }),
+);
+
+type NavigationItemProps = NavigationItemChildrenProps &
+  NavigationItemListChildrenProps;
+
+export const NavItemLinks = ({ links }: { links: NavigationListLinks[]}) => {
+  return (
+    <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+      {links?.map(({ title, href, description }) => (
+        <NavigationListItem key={title} href={href} title={title}>
+          {description}
+        </NavigationListItem>
+      ))}
+    </ul>
+  );
+};
 
 
-type NavigationItemProps = NavigationItemChildrenProps & NavigationItemListChildrenProps;
-
-export const NavigationItem = ({ children, title, links }:  NavigationItemProps) => {
-
+export const NavigationItem = ({
+  children,
+  title,
+  links,
+}: NavigationItemProps) => {
   const navContent = useMemo(() => {
     if (children) {
       return children;
     }
 
-    return (
-      <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-        {links?.map(({ title, href, description }) => (
-          <NavigationListItem key={title} href={href} title={title}>
-            {description}
-          </NavigationListItem>
-        ))}
-      </ul>
-    );
+    return <NavItemLinks links={links} />;
   }, [children, links]);
 
-
   return (
-    <li>
-      <NavigationMenuItem>
-        <NavigationMenuTrigger>{title}</NavigationMenuTrigger>
-        <NavigationMenuContent>{navContent}</NavigationMenuContent>
-      </NavigationMenuItem>
-    </li>
+    <NavigationMenuItem>
+      <NavigationMenuTrigger>{title}</NavigationMenuTrigger>
+      <NavigationMenuContent>{navContent}</NavigationMenuContent>
+    </NavigationMenuItem>
   );
 };
 
